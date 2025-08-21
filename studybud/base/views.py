c8272e8d022e5bf2d89d8dc4040cc1f3ad9a600a
GauravChanda7 <gauravchanda2022@gmail.com>
2025-08-21 14:59:15 +0000

    Add search & user login, logout & restricted pages
    Added search feature by username, host name or topics. Also added login
    and logout using sessions. Also restricted users from entering pages
    where authorisation is required using @login_required decorator

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,20 +1,66 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from django.contrib import messages
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from .models import Room, Topic
 from .forms import RoomForm
 
 # Create your views here.
 
+def login_page(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username = username, password = password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or password not exist')
+            return redirect('login')
+        
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
+
+
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {'rooms': rooms}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(Q(topic__name__icontains = q) |
+                                Q(name__icontains = q) |
+                                Q(description__icontains = q) |
+                                Q(host__username__icontains = q.strip('@'))
+                                )
+    topics = Topic.objects.all()
+    room_count = rooms.count()
+
+    context = {'rooms': rooms,
+               'topics': topics,
+               'room_count': room_count,
+               }
     return render(request, 'base/home.html', context)
+
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
 
+
+@login_required(login_url='login')
 def create_room(request):
     form = RoomForm()
 
@@ -27,9 +73,14 @@ def create_room(request):
     context = {'form' : form}
     return render(request, 'base/room_form.html', context)
 
+
+@login_required(login_url='login')
 def update_room(request, pk):
     room = Room.objects.get(id = pk)
     form = RoomForm(instance=room)
+
+    if request.user != room.host:
+        return HttpResponse('You are not allowed here!')
 
     if request.method == 'POST':
         form = RoomForm(request.POST, instance=room)
@@ -40,8 +91,13 @@ def update_room(request, pk):
     context = {'form' : form}
     return render(request, 'base/room_form.html', context)
 
+
+@login_required(login_url='login')
 def delete_room(request, pk):
     room = Room.objects.get(id = pk)
+
+    if request.user != room.host:
+        return HttpResponse('You are not allowed here')
 
     if request.method == 'POST':
         room.delete()
